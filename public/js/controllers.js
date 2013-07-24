19267e5b543d882a40d6fec3a8b2c6fea752df89
Soumendra Daas <daas.soumendra@gmail.com>
2013-07-24 21:51:23 +0530

    Added handling for activity selection

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,40 +1,78 @@
 
-var app = angular.module('MyApp', []);
+// Declare Underscore itself as a module
+var underscore = angular.module('underscore', []);
+underscore.factory('_', function() {
+  return window._; // assumes underscore has already been loaded on the page
+});
+
+var app = angular.module('MyApp', ['underscore']);
 
 
-app.controller('MyController', function($scope ){
+app.controller('MyController', function($scope, $http, _ ){
 
 	$scope.displayGroup = [
 		{ 
 			"label" : "May 2013",  
 			"id" : "201305",
 			"activity" : [
-				{	"summary" : { "mmdd" : "05/08", "duration" : 40 } },
-				{	"summary" : { "mmdd" : "05/12", "duration" : 41 } },
-				{	"summary" : { "mmdd" : "05/15", "duration" : 27 } },
-				{	"summary" : { "mmdd" : "05/18", "duration" : 31 } },
-				{	"summary" : { "mmdd" : "05/27", "duration" : 45 } }
+				{	
+					"id" : "201305082315",
+					"summary" : { "mmdd" : "05/08", "duration" : 40, "maxhr" : 150 } 
+				},
+				{	
+					"id" : "201305121815",
+					"summary" : { "mmdd" : "05/12", "duration" : 41, "maxhr" : 165 } 
+				},
+				{	
+					"id" : "201305150945",
+					"summary" : { "mmdd" : "05/15", "duration" : 27, "maxhr" : 170 } 
+				},
+				{	
+					"id" : "201305181904",
+					"summary" : { "mmdd" : "05/18", "duration" : 31, "maxhr" : 175 } 
+				}
 			]
 		},
 		{ 
 			"label" : "July 2013",  
 			"id" : "201307",
 			"activity" : [
-				{	"summary" : { "mmdd" : "07/23", "duration" : 35 } },
-				{	"summary" : { "mmdd" : "07/20", "duration" : 40 } },
-				{	"summary" : { "mmdd" : "07/18", "duration" : 34 } },
-				{	"summary" : { "mmdd" : "07/15", "duration" : 38 } },
-				{	"summary" : { "mmdd" : "07/13", "duration" : 42 } }
+				{	
+					"id" : "201307231800",
+					"summary" : { "mmdd" : "07/23", "duration" : 35, "maxhr" : 145 } 
+				},
+				{	
+					"id" : "201307201900",
+					"summary" : { "mmdd" : "07/20", "duration" : 40, "maxhr" : 165 } 
+				}
 			]
 		},
 		{ 
 			"label" : "June 2013",  
 			"id" : "201306",
 			"activity" : [
-				{	"summary" : { "mmdd" : "06/06", "duration" : 45 } },
-				{	"summary" : { "mmdd" : "06/15", "duration" : 30 } }
+				{	
+					"id" : "201306061000",
+					"summary" : { "mmdd" : "06/06", "duration" : 45, "maxhr" : 166 } 
+				},
+				{	
+					"id" : "201306150900",
+					"summary" : { "mmdd" : "06/15", "duration" : 30, "maxhr" : 170 } 
+				}
 			]
 		}
 	]
 
+	// handling when the user clicks on an activity 
+	$scope.activitySelected = function(dgId, activityId) {
+		console.log("dgid = " + dgId + ", Activity " + activityId + " selected");
+		var dg = _.find( $scope.displayGroup, function(dg) { return dg.id == dgId; });
+		if( dg != undefined ) console.log("found dg");
+		
+		var activity = _.find( dg.activity, function(x) { return x.id == activityId; });
+		if( activity != undefined ) console.log("found activity");
+
+		// mark this as a the selected or current activity	
+		$scope.currentActivity = activity;
+	}
 })
