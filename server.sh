ff4df24a1ba2d1e4b83fc234284bf840812c595c
Soumendra Daas <daas.soumendra@gmail.com>
2013-07-28 11:36:21 +0530

    added npm install to server.sh

--- server.sh
+++ server.sh
@@ -5,7 +5,16 @@
 # WATCHFOLDER for new .pwx and copies them into the DATAFOLDER when it finds a new one.
 export WATCHFOLDER="/Users/sdaas/Dropbox/Apps/WahooFitness"
 export DATAFOLDER="./data"
+export NPM="/opt/local/bin/npm"
 export NODE="/opt/local/bin/node"
+
+# Install all the NPM Dependencies.
+# Test if node_modules folder exists. If yes, assume that all node modules have
+# been installed
+if [ ! -d "node_modules" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  npm install
+fi
 
 # Copy all the PWX files from the drop box to the data folder
 find ${WATCHFOLDER} -name "*.pwx" -exec cp {} ${DATAFOLDER} \;
